79c7a69ff134f54e3db35e091cf24bd7952a1b6f
bobpruz <bobpruz@gmail.com>
2022-05-12 20:08:21 -0500

    added barrowed added reviews added seeds updated formatting

---
+++ controllers/api/barrowed-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { Barrowed } = require("../../models");
+const sequelize = require('../../config/connection');
+
+
+router.get('/', (req, res) => {
+    Barrowed.findAll()
+    .then(dbBarrowedData => res.json(dbBarrowedData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    if (req.session) {
+        Barrowed.create({
+            user_id: req.body.user_id,
+            book_id: req.body.book_id,
+        })
+        .then(dbBarrowedData => res.json(dbBarrowedData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Barrowed.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbBarrowedData => {
+        if (!dbBarrowedData) {
+            res.status(404).json({ message: 'No barrower found with this id' });
+            return;
+        }
+        res.json(dbBarrowedData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/api/reviews-routes.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const { Reviews } = require("../../models");
+const sequelize = require('../../config/connection');
+
+router.get('/', (req, res) => {
+    Reviews.findAll()
+    .then(dbReviewsData => res.json(dbReviewsData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Reviews.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbReviewsData => {
+            if (!dbReviewsData) {
+                res.status(404).json({ message: 'No review found with this id' });
+                return;
+            }
+            res.json(dbReviewsData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    if (req.session) {
+        Reviews.create({
+            review: req.body.review,
+            user_id: req.body.user_id,
+            book_id: req.body.book_id,
+            
+        })
+        .then(dbReviewsData => res.json(dbReviewsData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.put('/:id', (req, res) => {
+    Reviews.update(
+        {
+            review: req.body.review
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbReviewsData => {
+        if (!dbReviewsData) {
+            res.status(404).json({ message: 'No review found with this id' });
+            return;
+        }
+        res.json(dbReviewsData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Reviews.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbReviewsData => {
+        if (!dbReviewsData) {
+            res.status(404).json({ message: 'No review found with this id' });
+            return
+        }
+        res.json(dbReviewsData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
